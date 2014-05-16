eb54c0065fab7f55d1afb6e43f45b17c4ec18fd3
unknown <jparmenter@LT126634-JPARM.jomax.paholdings.com>
2014-05-16 13:16:44 -0700

    Updating my readme

--- README.md
+++ README.md
@@ -1,11 +1,16 @@
 Angular Bootstrap Demo
+======================
 
 Playing around with ui-angular's angular-bootstrap directives to mess around with angular UI and testing
 
 Build
 
+------------
 npm install
+------------
 
 Run
 
+-------------
 grunt serve
+-------------
--- test/client/spec/controllers/forms/form.js
+++ test/client/spec/controllers/forms/form.js
@@ -2,13 +2,9 @@
 
 describe('Controller: FormCtrl', function () {
 
-  // load the controller's module
   beforeEach(module('angularBootstrapApp'));
+  var FormCtrl, scope;
 
-  var FormCtrl,
-    scope;
-
-  // Initialize the controller and a mock scope
   beforeEach(inject(function ($controller, $rootScope) {
     scope = $rootScope.$new();
     FormCtrl = $controller('FormCtrl', {
