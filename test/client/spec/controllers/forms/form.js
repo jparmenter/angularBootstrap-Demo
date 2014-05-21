73c34d4c5d52d817b4a780d2316f0fa273039781
Jeremy Parmenter <shabado9889@gmail.com>
2014-05-20 23:08:59 -0700

    added form tests

--- test/client/spec/controllers/forms/form.js
+++ test/client/spec/controllers/forms/form.js
@@ -37,10 +37,39 @@ describe('Controller: FormCtrl', function () {
   });
 
   it('should attach a pass flag as false', function () {
-    expect(scope.fail).toBe(false);
+    expect(scope.pass).toBe(false);
+  });
+
+  it('should attach an initialized users', function() {
+    expect(scope.users).not.toBe(null);
+  });
+
+  it('should attach empty users', function() {
+    expect(scope.users.length).toBe(0);
   });
 
   it('should attach a isValidUser method that initiallly would return false', function () {
     expect(scope.isValidUser()).toBe(false);
+  });
+
+  it('should set fail to try when calling submit', function() {
+    scope.submit();
+    expect(scope.fail).toBe(true);
+  });
+
+  it('should set pass with user that has values', function() {
+    scope.user.firstName = 'foo';
+    scope.user.lastName = 'bar';
+    scope.user.acceptTerms = true;
+    scope.submit();
+    expect(scope.pass).toBe(true);
+  });
+
+  it('should add user to users after submit', function() {
+    scope.user.firstName = 'foo';
+    scope.user.lastName = 'bar';
+    scope.user.acceptTerms = true;
+    scope.submit();
+    expect(scope.users.length).toBe(1);
   });
 });
